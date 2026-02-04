d95778c11abed60c6c5422c5feedd34efe9fb197
hana <81144685+2501babe@users.noreply.github.com>
2026-02-03 22:37:55 -0800

    minimum delegation done. wow that was easy! (joke)

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "minimum_delegation_1sol")]
 use solana_native_token::LAMPORTS_PER_SOL;
 
 pub mod helpers;
@@ -6,16 +7,6 @@ pub mod processor;
 pub mod entrypoint;
 
 solana_pubkey::declare_id!("Stake11111111111111111111111111111111111111");
-
-// placeholders for features
-// we have ONE feature in the current stake program we care about:
-// * stake_raise_minimum_delegation_to_1_sol /
-//   9onWzzvCzNC2jfhxxeqRgs5q7nFAAKpCUvkj6T6GJK9i this may or may not be
-//   activated by time we are done, but it should be confined to the program so
-//   we use a placeholder for now to call it out. but we can just change the
-//   program. it is unclear if or when it will ever be activated, because it
-//   requires a validator vote
-const FEATURE_STAKE_RAISE_MINIMUM_DELEGATION_TO_1_SOL: bool = false;
 
 // feature_set::reduce_stake_warmup_cooldown changed the warmup/cooldown from
 // 25% to 9%. a function is provided by the sdk,
@@ -32,12 +23,15 @@ const PERPETUAL_NEW_WARMUP_COOLDOWN_RATE_EPOCH: Option<u64> = Some(0);
 /// NOTE: This is also used to calculate the minimum balance of a delegated
 /// stake account, which is the rent exempt reserve _plus_ the minimum stake
 /// delegation.
+#[cfg(feature = "minimum_delegation_1sol")]
 #[inline(always)]
 pub fn get_minimum_delegation() -> u64 {
-    if FEATURE_STAKE_RAISE_MINIMUM_DELEGATION_TO_1_SOL {
-        const MINIMUM_DELEGATION_SOL: u64 = 1;
-        MINIMUM_DELEGATION_SOL * LAMPORTS_PER_SOL
-    } else {
-        1
-    }
+    const MINIMUM_DELEGATION_SOL: u64 = 1;
+    MINIMUM_DELEGATION_SOL * LAMPORTS_PER_SOL
+}
+
+#[cfg(not(feature = "minimum_delegation_1sol"))]
+#[inline(always)]
+pub fn get_minimum_delegation() -> u64 {
+    1
 }
