03a7fbafae3f4760e3fc6ccd1afbd50b43a4d400
Gabe Rodriguez <grod220@gmail.com>
2026-09-14 18:23:35 +0200

    bump to 1.89 & use assume_init()

--- interface/src/tools.rs
+++ interface/src/tools.rs
@@ -61,7 +61,7 @@ pub fn get_vote_state(vote_account_info: &AccountInfo) -> Result<Box<VoteStateV4
         vote_account_info.key,
     )
     .map_err(|_| ProgramError::InvalidAccountData)?;
-    let vote_state = unsafe { Box::from_raw(Box::into_raw(vote_state).cast::<VoteStateV4>()) };
+    let vote_state = unsafe { vote_state.assume_init() };
 
     Ok(vote_state)
 }
